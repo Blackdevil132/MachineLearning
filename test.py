bc103dd27ecccc8fbec961774fbfb40ff3920e03
admin <lars@homeserver.fritz.box>
2019-04-24 09:38:57 +0000

    minor change

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from QRL import QRL
 
 qrl = QRL(0,0,0,0)
 qrl.loadFromFile("qtables/saved/190424_11")
-print(qrl.qtable.get(bytes((14, 15))))
+print(qrl.qtable.get(bytes((6, 7))))
 
 qrl.test(render=True)
